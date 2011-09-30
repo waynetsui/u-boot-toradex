48dfb3431fe6a8526dc53c6dd4049eef89104b23
Simon Glass <sjg@chromium.org>
2011-09-29 17:03:25 -0700

    tegra3: config: Tidy up tegra3-common.h to be closer to tegra2-common.h
    Quite a few changes have been made in config since the tegra3 header was
    forked off in May. This brings this file back into line.
    
    BUG=chromium-os:21033
    TEST=build and boot on seaboard
    
    Change-Id: I5244e03ca6a562f09b23bc9f0c11a5019084e399
    Reviewed-on: http://gerrit.chromium.org/gerrit/8695
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Tested-by: Simon Glass <sjg@chromium.org>

--- include/configs/tegra3-common.h
+++ include/configs/tegra3-common.h
@@ -25,71 +25,9 @@
 #define __TEGRA3_COMMON_H
 #include <asm/sizes.h>
 
-/*
- * QUOTE(m) will evaluate to a string version of the value of the macro m
- * passed in.  The extra level of indirection here is to first evaluate the
- * macro m before applying the quoting operator.
- */
-#define QUOTE_(m)	#m
-#define QUOTE(m)	QUOTE_(m)
+#include "tegra-common.h"
 
-/*
- * High Level Configuration Options
- */
-#define CONFIG_ARMCORTEXA9		/* This is an ARM V7 CPU core */
-#define CONFIG_TEGRA3			/* in a NVidia Tegra3 core */
-#define CONFIG_MACH_TEGRA_GENERIC	/* which is a Tegra generic machine */
-#define CONFIG_SYS_NO_L2CACHE		/* No L2 cache */
-#define CONFIG_BOARD_EARLY_INIT_F
-
-#include <asm/arch/tegra.h>		/* get chip and board defs */
-
-#define CACHE_LINE_SIZE			32
-
-/*
- * Display CPU and Board information
- */
-#define CONFIG_DISPLAY_CPUINFO
-#define CONFIG_DISPLAY_BOARDINFO
-
-#define CONFIG_SKIP_LOWLEVEL_INIT
-
-#define CONFIG_CMDLINE_TAG		/* enable passing of ATAGs */
-
-/*
- * Size of malloc() pool
- */
-#define CONFIG_SYS_MALLOC_LEN		SZ_4M	/* 4MB  */
-
-/*
- * PllX Configuration
- */
-#define CONFIG_SYS_CPU_OSC_FREQUENCY	1000000	/* Set CPU clock to 1GHz */
-
-/*
- * NS16550 Configuration
- */
-#define CONFIG_SERIAL_MULTI
-#define CONFIG_NS16550_BUFFER_READS
-#define CONFIG_SYS_NS16550
-#define CONFIG_SYS_NS16550_REG_SIZE	(-4)
-
-#define V_NS16550_CLK			216000000	/* 216MHz (pllp_out0) */
-
-#define CONFIG_SYS_NS16550_SERIAL
-#define CONFIG_SYS_NS16550_CLK		V_NS16550_CLK
-
-/*
- * select serial console configuration
- */
-#define CONFIG_CONS_INDEX		1
-
-/* allow to overwrite serial and ethaddr */
-#define CONFIG_ENV_OVERWRITE
-#define CONFIG_BAUDRATE			115200
-#define CONFIG_SYS_BAUDRATE_TABLE	{4800, 9600, 19200, 38400, 57600,\
-					115200}
-#define CONFIG_NET_MULTI
+#define CONFIG_TEGRA3			/* NVidia Tegra3 core */
 
 /* include default commands */
 #include <config_cmd_default.h>
@@ -100,9 +38,9 @@
 #undef CONFIG_CMD_IMI
 #undef CONFIG_CMD_IMLS
 #undef CONFIG_CMD_NFS		/* NFS support */
+#undef CONFIG_CMD_NET
 
 #define CONFIG_CMD_CACHE
-#define CONFIG_CMD_TIME
 
 /* turn on command-line edit/hist/auto */
 #define CONFIG_CMDLINE_EDITING
@@ -111,55 +49,24 @@
 
 #define CONFIG_SYS_NO_FLASH
 
-#define CONFIG_ENV_SIZE			SZ_4K
-
-/* Environment information */
-#define CONFIG_EXTRA_ENV_SETTINGS_COMMON \
-	CONFIG_STD_DEVICES_SETTINGS \
-	"console=ttyS0,115200n8\0" \
-	"smpflag=smp\0" \
-	"user=user\0" \
-	"serial#=1\0" \
-	"extra_bootargs=\0" \
-	"platform_extras=" TEGRA3_SYSMEM"\0" \
-	"videospec=tegrafb\0" \
-	"dev_extras=\0"
-
-#define CONFIG_LOADADDR		0x408000	/* def. location for kernel */
-#define CONFIG_BOOTDELAY	10		/* -1 to disable auto boot */
+/* Passed on the kernel command line to specify the console. */
+#define CONFIG_LINUXCONSOLE "console=ttyS0,115200n8"
 
 /*
- * Miscellaneous configurable options
+ * Defines the standard boot args; these are used in the vboot case (which
+ * doesn't run regen_all) as well as used as part of regen_all.
  */
-#define CONFIG_SYS_LONGHELP		/* undef to save memory */
-#define CONFIG_SYS_HUSH_PARSER		/* use "hush" command parser */
-#define CONFIG_SYS_PROMPT_HUSH_PS2	"> "
-#define CONFIG_SYS_PROMPT		V_PROMPT
-#define CONFIG_SILENT_CONSOLE
-/*
- * Increasing the size of the IO buffer as default nfsargs size is more
- *  than 256 and so it is not possible to edit it
- */
-#define CONFIG_SYS_CBSIZE		(256 * 2) /* Console I/O Buffer Size */
-/* Print Buffer Size */
-#define CONFIG_SYS_PBSIZE		(CONFIG_SYS_CBSIZE + \
-					sizeof(CONFIG_SYS_PROMPT) + 16)
-#define CONFIG_SYS_MAXARGS		32	/* max number of command args */
-/* Boot Argument Buffer Size */
-#define CONFIG_SYS_BARGSIZE		(CONFIG_SYS_CBSIZE)
-
-#define CONFIG_SYS_MEMTEST_START	(TEGRA_SDRC_CS0 + 0x600000)
-#define CONFIG_SYS_MEMTEST_END		(CONFIG_SYS_MEMTEST_START + 0x100000)
+#define CONFIG_BOOTARGS \
+	CONFIG_LINUXCONSOLE
 
 #define CONFIG_SYS_LOAD_ADDR		(0x80A00800)	/* default */
-#define CONFIG_SYS_HZ			1000
 
 /*-----------------------------------------------------------------------
  * Stack sizes
  *
  * The stack sizes are set up in start.S using the settings below
  */
-#define CONFIG_STACKBASE	0x2800000	/* 40MB */
+#define CONFIG_STACKBASE	(NV_PA_SDRAM_BASE + 0x2800000)	/* 40MB */
 #define CONFIG_STACKSIZE	0x20000		/* 128K regular stack*/
 
 /*-----------------------------------------------------------------------
@@ -171,17 +78,5 @@
 
 #define CONFIG_SYS_TEXT_BASE	0x80E08000
 #define CONFIG_SYS_SDRAM_BASE	PHYS_SDRAM_1
-
-#define CONFIG_SYS_INIT_RAM_ADDR	CONFIG_STACKBASE
-#define CONFIG_SYS_INIT_RAM_SIZE	CONFIG_SYS_MALLOC_LEN
-#define CONFIG_SYS_INIT_SP_ADDR		(CONFIG_SYS_INIT_RAM_ADDR + \
-						CONFIG_SYS_INIT_RAM_SIZE - \
-						GENERATED_GBL_DATA_SIZE)
-
-/*
- * 32M is what it takes the u-boot to allocate enough room for the kernel
- * loader to inflate the kernel and keep a copy of the device tree handy.
- */
-#define CONFIG_SYS_BOOTMAPSZ	SZ_32M
 
 #endif /* __TEGRA3_COMMON_H */

d0028e59053e34f60811296abf39f096cc437d2b
Max Krummenacher <max.krummenacher@toradex.com>
2014-05-06 12:53:32 +0200

    ARM: mx6: CPU errata and I2C changes
    follow commit 02824dc78642b3057cc8c1ab7dc32203f55a17fa
    ARM: mx6: Update non-Freescale boards to include CPU errata.
    
    follow commit b089d039b1971fc3abfe1d9bcebd0d35245fb110
    i2c: update config using mxc driver to new subsystem

--- include/configs/apalis-imx6.h
+++ include/configs/apalis-imx6.h
@@ -10,6 +10,7 @@
 #ifndef __CONFIG_H
 #define __CONFIG_H
 
+#include "mx6_common.h"
 #define CONFIG_MX6
 #define CONFIG_APALIS_IMX6 /* Toradex Apalis iMX6 module */
 #define CONFIG_DISPLAY_CPUINFO
@@ -44,8 +45,8 @@
 
 /* I2C Configs */
 #define CONFIG_CMD_I2C
-#define CONFIG_I2C_MULTI_BUS
-#define CONFIG_I2C_MXC
+#define CONFIG_SYS_I2C
+#define CONFIG_SYS_I2C_MXC
 #define CONFIG_SYS_I2C_SPEED		100000
 
 /* OCOTP Configs */

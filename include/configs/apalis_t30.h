0d262f71a183f17ec04e5585569a6f2802f98930
Marcel Ziswiler <marcel@ziswiler.com>
2014-10-11 00:24:32 +0200

    tegra: apalis_t30: master revamp
    Update Apalis T30 as per the following commits
    
    c369139234c03b1494394d12cd27009f47aa6606
    tegra: dts: Add serial port details
    
    461be2f96e4b87e5065208c6659a47dd0ad9e9f8
    kconfig: remove redundant "string" type in arch and board Kconfigs
    
    f1ef2b62339526df3b921bcfefd174ce76d4c624
    kconfig: move CONFIG_DEFAULT_DEVICE_TREE to kconfig
    
    Signed-off-by: Marcel Ziswiler <marcel@ziswiler.com>

--- include/configs/apalis_t30.h
+++ include/configs/apalis_t30.h
@@ -11,11 +11,6 @@
 
 #include "tegra30-common.h"
 
-/* Enable fdt support for Apalis T30. Flash the image in u-boot-dtb.bin */
-#define CONFIG_DEFAULT_DEVICE_TREE	tegra30-apalis
-#define CONFIG_OF_CONTROL
-#define CONFIG_OF_SEPARATE
-
 /* High-level configuration options */
 #define V_PROMPT			"Apalis T30 # "
 #define CONFIG_TEGRA_BOARD_STRING	"Toradex Apalis T30"

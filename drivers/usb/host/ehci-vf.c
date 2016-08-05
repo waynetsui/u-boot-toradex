dfc215b18aefab24e72e031029a39113508735dc
Sanchayan Maity <maitysanchayan@gmail.com>
2016-08-05 13:59:11 +0530

    usb: host: ehci-vf: Implement board_usb_phy_mode weak function
    Add board_usb_phy_mode weak function on similar lines to ehci-mx6.
    However since Vybrid USB does not have a true OTG, make this weak
    functon just return 0. The function is supposed to be implemented
    by the individual boards using a GPIO for providing the OTG pin
    functionality.
    
    Signed-off-by: Sanchayan Maity <maitysanchayan@gmail.com>
    Signed-off-by: Stefan Agner <stefan.agner@toradex.com>

--- drivers/usb/host/ehci-vf.c
+++ drivers/usb/host/ehci-vf.c
@@ -161,6 +161,11 @@ static void usb_oc_config(int index)
 	__raw_writel(val, ctrl);
 }
 
+int __weak board_usb_phy_mode(int port)
+{
+	return 0;
+}
+
 int __weak board_ehci_hcd_init(int port)
 {
 	return 0;
@@ -170,6 +175,7 @@ int ehci_hcd_init(int index, enum usb_init_type init,
 		struct ehci_hccr **hccr, struct ehci_hcor **hcor)
 {
 	struct usb_ehci *ehci;
+	enum usb_init_type type;
 
 	if (index >= ARRAY_SIZE(nc_reg_bases))
 		return -EINVAL;
@@ -187,6 +193,10 @@ int ehci_hcd_init(int index, enum usb_init_type init,
 	*hccr = (struct ehci_hccr *)((uint32_t)&ehci->caplength);
 	*hcor = (struct ehci_hcor *)((uint32_t)*hccr +
 			HC_LENGTH(ehci_readl(&(*hccr)->cr_capbase)));
+
+	type = board_usb_phy_mode(index);
+	if (type != init)
+		return -ENODEV;
 
 	if (init == USB_INIT_DEVICE) {
 		setbits_le32(&ehci->usbmode, CM_DEVICE);

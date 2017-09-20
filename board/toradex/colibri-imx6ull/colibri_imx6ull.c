a24747ec8b63ed5d5bc4fc10ee1b15ab5f2ca8a0
Stefan Agner <stefan.agner@toradex.com>
2017-09-20 13:17:53 -0700

    colibri-imx6ull: make sure MTD partitions get passed
    Use the correct compatible string for the i.MX6ULL GPMI device
    tree node to make sure MTD partition passing to the Linux kernel
    works.
    
    Signed-off-by: Stefan Agner <stefan.agner@toradex.com>
    Acked-by: Max Krummenacher <max.krummenacher@toradex.com>

--- board/toradex/colibri-imx6ull/colibri_imx6ull.c
+++ board/toradex/colibri-imx6ull/colibri_imx6ull.c
@@ -437,7 +437,7 @@ int ft_board_setup(void *blob, bd_t *bd)
 {
 #if defined(CONFIG_FDT_FIXUP_PARTITIONS)
 	static struct node_info nodes[] = {
-		{ "fsl,imx7d-gpmi-nand", MTD_DEV_TYPE_NAND, }, /* NAND flash */
+		{ "fsl,imx6ull-gpmi-nand", MTD_DEV_TYPE_NAND, }, /* NAND flash */
 	};
 
 	/* Update partition nodes using info from mtdparts env var */

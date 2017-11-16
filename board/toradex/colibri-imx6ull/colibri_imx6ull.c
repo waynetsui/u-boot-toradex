5e54aedd1c5f04ed3efe45c50ea79c3350a86bfc
Max Krummenacher <max.krummenacher@toradex.com>
2017-11-16 20:26:57 +0100

    colibri_imx6ull.c: remove dead code
    Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>
    Acked-by: Stefan Agner <stefan.agner@toradex.com>

--- board/toradex/colibri-imx6ull/colibri_imx6ull.c
+++ board/toradex/colibri-imx6ull/colibri_imx6ull.c
@@ -27,8 +27,6 @@
 #include <miiphy.h>
 #include <mtd_node.h>
 #include <netdev.h>
-#include <power/pmic.h>
-#include <power/rn5t567_pmic.h>
 #include <usb.h>
 #include <usb/ehci-ci.h>
 #include "../common/tdx-common.h"
@@ -368,62 +366,6 @@ int board_late_init(void)
 
 	return 0;
 }
-
-#ifdef CONFIG_DM_PMIC
-int power_init_board(void)
-{
-	struct udevice *dev;
-	int reg, ver;
-	int ret;
-
-
-	ret = pmic_get("rn5t567", &dev);
-	if (ret)
-		return ret;
-	ver = pmic_reg_read(dev, RN5T567_LSIVER);
-	reg = pmic_reg_read(dev, RN5T567_OTPVER);
-
-	printf("PMIC:  RN5T567 LSIVER=0x%02x OTPVER=0x%02x\n", ver, reg);
-
-	/* set judge and press timer of N_OE to minimal values */
-	pmic_clrsetbits(dev, RN5T567_NOETIMSETCNT, 0x7, 0);
-
-	/* configure sleep slot for 3.3V Ethernet */
-	reg = pmic_reg_read(dev, RN5T567_LDO1_SLOT);
-	reg = (reg & 0xf0) | reg >> 4;
-	pmic_reg_write(dev, RN5T567_LDO1_SLOT, reg);
-
-	/* disable DCDC2 discharge to avoid backfeeding through VFB2 */
-	pmic_clrsetbits(dev, RN5T567_DC2CTL, 0x2, 0);
-
-	/* configure sleep slot for ARM rail */
-	reg = pmic_reg_read(dev, RN5T567_DC2_SLOT);
-	reg = (reg & 0xf0) | reg >> 4;
-	pmic_reg_write(dev, RN5T567_DC2_SLOT, reg);
-
-	/* disable LDO2 discharge to avoid backfeeding from +V3.3_SD */
-	pmic_clrsetbits(dev, RN5T567_LDODIS1, 0x2, 0);
-
-	return 0;
-}
-
-void reset_cpu(ulong addr)
-{
-	struct udevice *dev;
-
-	pmic_get("rn5t567", &dev);
-
-	/* Use PMIC to reset, set REPWRTIM to 0 and REPWRON to 1 */
-	pmic_reg_write(dev, RN5T567_REPCNT, 0x1);
-	pmic_reg_write(dev, RN5T567_SLPCNT, 0x1);
-
-	/*
-	 * Re-power factor detection on PMIC side is not instant. 1ms
-	 * proved to be enough time until reset takes effect.
-	 */
-	mdelay(1);
-}
-#endif
 
 int checkboard(void)
 {
